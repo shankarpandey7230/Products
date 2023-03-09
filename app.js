51241dc4c73d2e482b3cf7b85d08a7ef87db5a89
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-03-09 22:05:06 +1100

    products loading and error setup

--- app.js
+++ app.js
@@ -1,77 +1,25 @@
-let filteredProducts = [...products];
+const url = "https://course-api.com/javascript-store-products";
 
+const productsDOM = document.querySelector(".products-center");
 
+const fetchProducts = async () => {
+  productsDOM.innerHTML = `<div class="loading"></div>`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(data);
+    return data;
+  } catch (error) {
+    // console.log(error);
+    productsDOM.innerHTML = `<p class="error">there was an error</p>`;
+  }
+};
+const displayProducts = (list) => {
+  console.log(list);
+};
 
-
-const productsContainer = document.querySelector('.products-container');
-
-const displayProducts =()=>{
-    //if statement
-    if(filteredProducts.length<1){
-        productsContainer.innerHTML= `<h6>Sorry, Your search does not match </h6>`;
-        return;
-    }
-
-
-    productsContainer.innerHTML = filteredProducts.map(({id,title, image, price})=>{
-        return` <article class="product" data-id="${id}">
-        
-        <img src="${image}" class="product-img img"
-         />
-        <footer>
-            <h5 class="product-name">${title}</h5>
-        <span class="product-price">$ ${price}</span>
-        </footer>
-            </article>
-`
-        
-    }).join('');
-}
-displayProducts();
-//Text filter
-const form = document.querySelector('.input-form');
-const searchInput = document.querySelector('.search-input');
-
-form.addEventListener('keyup',()=>{
-    const inputValue = searchInput.value;
-    // console.log(inputValue);
-    filteredProducts = products.filter((product)=>{
-        return product.title.toLowerCase().includes(inputValue);
-    });
-    displayProducts();
-} )
-
-// filter buttons
-
-const companiesDOM = document.querySelector('.companies');
-
-const displayButtons =()=>{
-    const buttons =['all' ,... new Set(products.map((product) => product.company))];
-    // console.log(buttons);
-    companiesDOM.innerHTML = buttons.map((company)=>{
-        return`<button class="company-btn" data-id =${company}>${company}</button>`
-    }).join('');
-}
-displayButtons();
-
-
-// events on companies
-
-companiesDOM.addEventListener('click',(e)=>{
-    // console.log(e.target);
-    const el = e.target;
-    if(el.classList.contains('company-btn')){
-        if(el.dataset.id === 'all'){
-            filteredProducts = [...products];
-        }
-        else{
-            filteredProducts = products.filter((product)=>{
-                return product.company === el.dataset.id;
-
-            });
-
-        }
-        searchInput.value ='';
-        displayProducts();
-    }
-});
+const start = async () => {
+  const data = await fetchProducts();
+  displayProducts(data);
+};
+start();

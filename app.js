95dac5b8740186f171054b893b4a4cb207849cbd
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-03-15 09:18:50 +1100

    displaying products

--- app.js
+++ app.js
@@ -3,23 +3,42 @@ const url = "https://course-api.com/javascript-store-products";
 const productsDOM = document.querySelector(".products-center");
 
 const fetchProducts = async () => {
-  productsDOM.innerHTML = `<div class="loading"></div>`;
+  productsDOM.innerHTML = '<div class="loading"></div>';
   try {
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.log(data);
+    const resp = await fetch(url);
+    const data = await resp.json();
     return data;
   } catch (error) {
-    // console.log(error);
-    productsDOM.innerHTML = `<p class="error">there was an error</p>`;
+    productsDOM.innerHTML = '<p class="error">there was an error</p>';
   }
 };
+
 const displayProducts = (list) => {
-  console.log(list);
+  const productList = list
+    .map((product) => {
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const { url: img } = product.fields.image[0];
+      const formatPrice = price / 100;
+      // id,name,price,img
+      return `<a class="single-product" href="products.html">
+            <img src="${img}" class="single-product-img img" alt="${title}" />
+            <footer>
+              <h5 class="name">${title}</h5>
+              <span class="price">$${formatPrice}</span>
+            </footer>
+          </a>`;
+    })
+    .join("");
+  productsDOM.innerHTML = ` <div class="products-container">
+         ${productList}
+          
+        </div>`;
 };
 
 const start = async () => {
   const data = await fetchProducts();
   displayProducts(data);
 };
+
 start();
